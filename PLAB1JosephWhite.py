9352367bdb4bdc4cf7a277a070806f61617f4bc3
WhiteJ9883 <whitej9883@student.faytechcc.edu>
2025-04-02 11:41:12 -0400

    First submission
    hi!

---
+++ PLAB1JosephWhite.py
@@ -0,0 +1,8 @@
+# Joseph White
+# 4/2/2025
+# PLAB1
+# Writing a simple program to display someones name.
+
+firstname = input('What is your first name?' )
+lastname = input('What is your last name?' )
+print ("Hello "+firstname +lastname)
